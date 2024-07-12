9d8561a332ceabe74caff0ef1bee1248d24a7f95
XAzure 16 <21864847-XAzure16@users.noreply.gitlab.com>
2024-07-12 22:20:59 +0200

    Patch 1

---
+++ main.py
@@ -0,0 +1,15 @@
+import nextcord
+from nextcord.ext import commands
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv()
+
+intents = nextcord.intents.all()
+bot = commands.Bot(command_prefix=".", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Bot is ready!")
+
+bot.run(environ.get("DISCORD_TOKEN"))
